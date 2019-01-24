da61662383be1881fe576edcbbb16185af1d40ca
Olivia Wan <wan.o@husky.neu.edu>
2019-01-24 14:40:46 -0500

    configurations for systemd job to start app on startup

--- start.sh
+++ start.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-#if [[ "x$PROD" == "x" ]]; then
-#	echo "This script is for starting in production."
-#	echo "Use"
-#	echo "   mix phx.server"
-#	exit
-#fi
-
 export MIX_ENV=prod
 export PORT=4790
 
@@ -16,8 +9,4 @@ _build/prod/rel/practice/bin/practice stop || true
 
 echo "Starting app..."
 
-_build/prod/rel/practice/bin/practice start
-
-# TODO: Add a cron rule or systemd service file
-#       to start your app on system boot.
-
+_build/prod/rel/practice/bin/practice foreground
